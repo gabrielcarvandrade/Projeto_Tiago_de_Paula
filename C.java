aa9aaeb40ec9a60d704782ee5bc7d882e6fb06f3
Tiago de Paula <brtiagomartins@gmail.com>
2024-10-16 20:04:16 -0300

    Atualiza Classe A e adiciona Classe C

---
+++ C.java
@@ -0,0 +1,31 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    // Getters
+    public String getC1() {
+        return C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    // Setters
+    public void setC1(String c1) {
+        this.C1 = c1;
+    }
+
+    public void setC2(int c2) {
+        this.C2 = c2;
+    }
+
+    // Métodos MC1 e MC2
+    public void MC1() {
+        System.out.println("Método MC1 chamado");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2 chamado");
+    }
+}
